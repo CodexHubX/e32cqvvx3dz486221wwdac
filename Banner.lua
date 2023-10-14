fa58afd22786d74898b43f0a4c481b6ea414e527
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2023-10-15 03:14:20 +0700

    Create Banner.lua

---
+++ Banner.lua
@@ -0,0 +1,14 @@
+BannerSpacail = {
+[1] = 'AVA',
+[2] = 'AVA',
+[3] = 'AVA',
+}
+BannerStandard = {
+[1] = 'spedwagon',
+[2] = 'Bulma',
+[3] = 'Geto',
+[4] = 'Ver',
+[5] = 'Ccd',
+[6] = 'AVA',
+}
+ Time = 3
