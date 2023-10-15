37c91e1a8f4a0abe8190a1168a480fae1e2ecb48
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2023-10-16 06:17:47 +0700

    Update Banner.lua

--- Banner.lua
+++ Banner.lua
@@ -1,14 +1,1 @@
-BannerSpacail = {
-[1] = 'vegeta_super',
-[2] = 'king_7ds',
-[3] = 'frieza_golden',
-}
-BannerStandard = {
-[1] = 'sakura',
-[2] = 'josuke',
-[3] = 'nezuko',
-[4] = 'genos',
-[5] = 'giorno',
-[6] = 'uryu',
-}
- TimeInBanner = '06'
+
